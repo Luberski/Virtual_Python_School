16239d76b18ef42ba3782b2f45c06cc2f1027661
Luberski <luberski@int.pl>
2021-12-02 21:05:48 +0100

    mini-zmiany w logowanku

--- Back-End/app.py
+++ Back-End/app.py
@@ -6,19 +6,21 @@ app = Flask(__name__)
 
 ipa_ = ipahttp.ipa('ipa2.zut.edu.pl')
 
-@app.route("/login", methods=['POST','GET'])
+@app.route("/login", methods=['POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if ipa_.login(request.form['username'],
-                      request.form['password']) != None:
-            return json.dumps(ipa_.user_show(request.form['username']), indent=4)
+
+    if request.method == 'POST' and request.is_json:
+        post_data = request.get_json()
+        if ipa_.login(post_data['username'],
+                      post_data['password']) != None:
+            return json.dumps(ipa_.user_show(post_data['username']), indent=4)
         else:
-            error = 'POST REQUEST'
+            error = 'Error reading json'
     return str(error)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world</p>"
+    return "Hello world"
 
 app.run()
