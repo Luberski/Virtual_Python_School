056d5f1c8daf927cd2288ab6f41921fa7abfc108
ssyuqixe <59923174+ssyuqixe@users.noreply.github.com>
2021-12-08 16:09:37 +0100

    jwt
    ktory "nie dziala"

--- Back-End/app.py
+++ Back-End/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_jwt import JWT, jwt_required, current_identity
 import ipahttp
 import json
 import user_parser as up
+from werkzeug.security import safe_str_cmp
+
 
 app = Flask(__name__)
 
 ipa_ = ipahttp.ipa('ipa2.zut.edu.pl')
+app.config['SECRET_KEY'] = 'super-secret'
+app.debug = True
 
 @app.route("/api/login", methods=['POST','GET'])
 def login():
@@ -13,15 +18,28 @@ def login():
     if request.method == 'POST':
         if ipa_.login(request.form['username'],
                       request.form['password']) != None:
-            json_dump = ipa_.user_show(request.form['username'])
-            tst = up.user(json_dump)
-            tst.parser()
-            tst.check()
-            return json_dump
-
+            user = up.user(ipa_.user_show(request.form['username']))
+            user.parser()
+           # tst.check()
+            return user.dicto
         else:
-            error = 'POST REQUEST'
+            error = '404 xD'
     return str(error)
+
+def authenticate(username, password):
+    return None
+
+def identity(payload):
+    #user_id = payload['identity']
+    return None
+
+jwt = JWT(app, authenticate, identity)
+@app.route('/api/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+
 
 
 app.run()
