c2fc022bf88ab797f811ec4d76a7faadb9bc9d56
Luberski <luberski@int.pl>
2021-12-13 13:38:07 +0100

    npm installed gidgud

--- Back-End/app/__init__.py
+++ Back-End/app/__init__.py
@@ -28,6 +28,11 @@ ipa_ = ipahttp.ipa('ipa2.zut.edu.pl')
 #             'user': user.format()
 #     })
 
+@app.route("/en/playground", methods=['POST','GET'])
+def parse():
+    data = request.data
+    print(data)
+
 @app.route("/login", methods=['POST'])
 def login():
     error = None
