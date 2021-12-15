d03164a6b89043706cde223d945ce73de7d49370
Luberski <luberski@int.pl>
2021-12-15 16:39:00 +0100

    playground json return fix

--- Back-End/app/sshrequests/sshrequests.py
+++ Back-End/app/sshrequests/sshrequests.py
@@ -5,7 +5,7 @@ class asd:
     def run_code(code):
         bashcmd = "python -c '{data}'".format(data=code[:-1])
         pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
-        text = pipe.communicate()[0]
-        err = pipe.communicate()[1]
+        text = pipe.communicate()[0].decode('ascii')
+        err = pipe.communicate()[1].decode('ascii')
         print(text,err)
         return jsonify({"return":text, "error":err})
