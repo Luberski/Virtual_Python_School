5fc981f59927899793c2dcdd7ae92219f635aa52
Luberski <luberski@int.pl>
2021-12-15 16:40:40 +0100

    playground json return fix

--- Back-End/app/sshrequests/sshrequests.py
+++ Back-End/app/sshrequests/sshrequests.py
@@ -3,7 +3,7 @@ from flask import jsonify
 
 class asd:
     def run_code(code):
-        bashcmd = "python -c '{data}'".format(data=code[:-1])
+        bashcmd = "python -c '{data}'".format(data=code)
         pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
         text = pipe.communicate()[0].decode('ascii')
         err = pipe.communicate()[1].decode('ascii')
