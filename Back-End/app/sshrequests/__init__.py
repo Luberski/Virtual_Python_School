dc72090b130254b47462759a53eab0454f24fc18
Luberski <luberski@int.pl>
2021-12-15 16:34:52 +0100

    asd

--- Back-End/app/sshrequests/__init__.py
+++ Back-End/app/sshrequests/__init__.py
@@ -1,3 +1,3 @@
-from .sshrequests import sshrequests
+from .sshrequests import *
 
 
--- Back-End/app/sshrequests/sshrequests.py
+++ Back-End/app/sshrequests/sshrequests.py
@@ -1,14 +1,11 @@
-# from subprocess import Popen, PIPE
-# from flask import jsonify, make_response
+from subprocess import Popen, PIPE
+from flask import jsonify
 
-# def parse_code(argument):
-#     with open('test2.txt', 'r') as file:
-#     data = file.read()
-
-#     bashcmd = "python -c '{data}'".format(data=data[:-1])
-#     pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
-#                                                                                                                             text = pipe.communicate()[0]
-#     err = pipe.communicate()[1]
-
-#     response = make_response(jsonify({"output": text, "error":err}))                                                        response.headers["Content-Type"] = "application/json"
-#     return response
+class asd:
+    def run_code(code):
+        bashcmd = "python -c '{data}'".format(data=code[:-1])
+        pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
+        text = pipe.communicate()[0]
+        err = pipe.communicate()[1]
+        print(text,err)
+        return jsonify({"return":text, "error":err})
