6095d868e255733ee4361098a1376cdb5d0fd9e0
Paweł Kałdus <xdk78888@gmail.com>
2021-12-28 15:38:00 +0100

    fix dark theme inputs

--- Front-End/src/components/Input/index.tsx
+++ Front-End/src/components/Input/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ label, register, required, ...rest }) => (
   <input
     {...register(label, { required })}
-    className="bg-gray-100 p-3 rounded-xl w-96"
+    className="bg-gray-100 dark:bg-gray-700 p-3 rounded-xl w-96"
     {...rest}
   />
 );
