458141e597c28082e2c6dc6c485b42478ffb571d
Luberski <luberski@int.pl>
2022-01-12 14:54:35 +0100

    Changed to execute scripts on container, not on machine

--- Back-End/app/pythonRunner/__init__.py
+++ Back-End/app/pythonRunner/__init__.py
@@ -3,7 +3,7 @@ from subprocess import Popen, PIPE
 
 class RemotePythonRunner:
     def run_code(self, code):
-        bashcmd = f"python -c '{code}'"
+        bashcmd = f"lxc exec first -- python -c '{code}'"
         pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
         text = pipe.communicate()[0].decode("ascii")
         err = pipe.communicate()[1].decode("ascii")
