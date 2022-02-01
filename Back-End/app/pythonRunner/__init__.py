2dbdeeefd2de3ec0808aea0de5f916d9a873f184
Luberski <Luberski@int.pl>
2022-02-01 17:03:59 +0100

    playground working?

--- Back-End/app/pythonRunner/__init__.py
+++ Back-End/app/pythonRunner/__init__.py
@@ -3,8 +3,11 @@ from subprocess import Popen, PIPE
 
 class RemotePythonRunner:
     def run_code(self, code):
-        bashcmd = f"~/test.sh test '{code}'"
+        bashcmd = "lxc exec test -- python -c '{}'".format(self.process(code))
         pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
         text = pipe.communicate()[0].decode("ascii")
         err = pipe.communicate()[1].decode("ascii")
         return text, err
+
+    def process(self, code):
+        return code.replace("\'", "\"")
