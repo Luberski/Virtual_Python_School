10156c8f4dabe80a714c228e9852e9c7d1c2a500
Luberski <Luberski@int.pl>
2022-02-01 17:50:27 +0100

    fixing parser

--- Back-End/app/pythonRunner/__init__.py
+++ Back-End/app/pythonRunner/__init__.py
@@ -5,10 +5,15 @@ from subprocess import Popen, PIPE
 # Wymyślić sposób jak wymierzyć czas żeby wywalać while nieskończone
 # tworzyć maszynę dla każdego użytkownika (napisać skrypt bashowy)
 # timeout na maszynę jesli nie używana przez jakiś czas
+# try except na parse
 
 class RemotePythonRunner:
     def run_code(self, code):
-        bashcmd = "lxc exec test -- python -c '{}'".format(self.parse(code))
+        code = self.parse(code)
+        if(code[0] == 1):
+            return code[1], ''
+
+        bashcmd = "lxc exec test -- python -c '{}'".format(self.parse(code[1]))
         pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
         text = pipe.communicate()[0].decode("ascii")
         err = pipe.communicate()[1].decode("ascii")
@@ -19,6 +24,6 @@ class RemotePythonRunner:
         
         for i in code.split(' '):
             if(i.lower() == 'import'):
-                return 'You cannot import modules!', ''
+                return 1 , "You cannot import modules!"
 
-        return code
+        return 0, code
