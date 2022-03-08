baf2be875226b092dfae52f35e71511ab8c82530
Paweł Kałdus <xdk78888@gmail.com>
2022-03-08 09:57:35 +0100

    refactor buttons

--- Front-End/src/components/Button/index.tsx
+++ Front-End/src/components/Button/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 import clsx from "clsx";
 
-type ButtonProps = {};
+export enum ButtonVariant {
+  PRIMARY = "btn-primary",
+  SECONDARY = "btn-secondary",
+  OUTLINE = "btn-outline",
+  OUTLINE_PRIMARY = "btn-outline-primary",
+  DANGER = "btn-danger",
+  SUCCESS = "btn-success",
+}
+
+export enum ButtonSize {
+  DEFAULT = "",
+  NORMAL = "w-24",
+  MEDIUM = "w-32",
+  LARGE = "w-40",
+  EXTRA_LARGE = "w-48",
+}
+
+type ButtonProps = {
+  variant?: ButtonVariant;
+  disabled?: boolean;
+  sizeType?: ButtonSize;
+};
 
 const Button = ({
   children,
   className,
   onClick,
+  variant = ButtonVariant.SECONDARY,
+  sizeType = ButtonSize.DEFAULT,
+  disabled = false,
+  type = "button",
+  ...props
 }: ButtonProps & React.HTMLProps<HTMLButtonElement>) => (
-  <button className={clsx("btn", className)} onClick={onClick}>
+  <button
+    className={clsx("btn", variant, sizeType, className)}
+    onClick={onClick}
+    disabled={disabled}
+    {...props}
+  >
     {children}
   </button>
 );
--- Front-End/src/components/ButtonLink/index.tsx
+++ Front-End/src/components/ButtonLink/index.tsx
@@ -1,18 +1,50 @@
 import React, { forwardRef } from "react";
 import clsx from "clsx";
 
-type ButtonLinkProps = {};
+export enum ButtonLinkVariant {
+  PRIMARY = "btn-primary",
+  SECONDARY = "btn-secondary",
+  OUTLINE = "btn-outline",
+  OUTLINE_PRIMARY = "btn-outline-primary",
+  DANGER = "btn-danger",
+  SUCCESS = "btn-success",
+}
+
+export enum ButtonLinkSize {
+  DEFAULT = "",
+  NORMAL = "w-24",
+  MEDIUM = "w-32",
+  LARGE = "w-40",
+  EXTRA_LARGE = "w-48",
+}
+
+type ButtonLinkProps = {
+  variant?: ButtonLinkVariant;
+  disabled?: boolean;
+  sizeType?: ButtonLinkSize;
+};
 
 const ButtonLink = forwardRef(function ButtonLink(
   {
     href,
     children,
     className,
+    variant = ButtonLinkVariant.SECONDARY,
+    sizeType = ButtonLinkSize.DEFAULT,
   }: ButtonLinkProps & React.HTMLProps<HTMLAnchorElement>,
   _ref
 ) {
   return (
-    <a href={href} className={clsx("btn", "hover:no-underline", className)}>
+    <a
+      href={href}
+      className={clsx(
+        "btn",
+        variant,
+        sizeType,
+        "hover:no-underline",
+        className
+      )}
+    >
       {children}
     </a>
   );
