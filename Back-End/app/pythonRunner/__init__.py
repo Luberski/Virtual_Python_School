bebad8d7a7c457bc679a71b25aae4888ea4cf50d
Luberski <luberski@int.pl>
2022-03-08 10:32:26 +0100

    playground tests

--- Back-End/app/pythonRunner/__init__.py
+++ Back-End/app/pythonRunner/__init__.py
@@ -14,12 +14,17 @@ class RemotePythonRunner:
         if(code[0] == 1):
             return code[1], ''
 
+        self.create_file(code=code)
         # bashcmd = "lxc exec test -- python -c '{}'".format(code[1])
         bashcmd = "lxc exec test -- python script.py"
         pipe = Popen(bashcmd, stdout=PIPE, stderr=PIPE, shell=True)
         text = pipe.communicate()[0].decode("ascii")
         err = pipe.communicate()[1].decode("ascii")
         return text, err
+
+    def create_file(self, imports, code):
+        bashcmd = "lxc exec test -- echo {} > script.py".format(code)
+        Popen(bashcmd, shell=True)
 
     def parse(self, code):
         code = code.replace("\'", "\"")
