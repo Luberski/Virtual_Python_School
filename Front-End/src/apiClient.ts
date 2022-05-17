3d675228ea27dc4c8975b4858f5e30224e8189ba
Paweł Kałdus <xdk78888@gmail.com>
2022-05-17 10:57:54 +0200

    try to fix cors

--- Front-End/src/apiClient.ts
+++ Front-End/src/apiClient.ts
@@ -1,6 +1,9 @@
 import ky from 'ky-universal';
 
-const BASE_URL = `http://localhost:3000/api`;
+let BASE_URL = `http://localhost:3000/api`;
+if (process.env.NODE_ENV === 'production') {
+  BASE_URL = `http://virtualschool.wi.zut.edu.pl/api`;
+}
 
 export default ky.extend({
   prefixUrl: BASE_URL,
