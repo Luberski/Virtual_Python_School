4fd93a862eab37b816b529330e0f45b830f5d63f
Mariusz Lubowicki <Luberski@protonmail.com>
2022-07-08 10:25:11 +0200

    Update PullandBuild.sh

--- PullandBuild.sh
+++ PullandBuild.sh
@@ -16,7 +16,7 @@ pip install -U -r /var/www/html/requirements.txt
 cd Front-End/
 npm i
 npm run build
-#pm2 start npm --name "virtual-school" -- start # Nie działa uruchomione przez shell script
+pm2 start npm --name "virtual-school" -- start
 
 cd ../Back-End/
-#gunicorn --daemon  --bind 0.0.0.0:5000 app:app
+gunicorn --daemon  --bind 0.0.0.0:5000 app:app
