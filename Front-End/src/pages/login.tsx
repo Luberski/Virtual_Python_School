eb271685b03ccb03a5fdba8f411dc8e985ffe639
Paweł Kałdus <xdk78888@gmail.com>
2022-08-07 14:21:41 +0200

    re-design login page and input component

--- Front-End/src/pages/login.tsx
+++ Front-End/src/pages/login.tsx
@@ -57,80 +57,80 @@ export default function LoginPage() {
       </Head>
       <div className="h-full w-full">
         <NavBar />
-        <div className="container my-6 mx-auto flex flex-col items-center justify-center px-6 pb-4">
-          <div>
-            <h1 className="text-center">{t('Auth.welcome-back')}</h1>
+        <div className="container my-6 mx-auto flex flex-col items-center justify-center rounded-lg bg-white p-9 shadow dark:bg-neutral-800">
+          <h1 className="text-center text-indigo-900 dark:text-indigo-300">
+            {t('Auth.welcome-back')}
+          </h1>
+          <div className="container mx-auto px-6 pt-6">
+            <form
+              className="flex flex-col items-center justify-center space-y-6"
+              onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                label="username"
+                type="username"
+                placeholder={t('Auth.username')}
+                register={register}
+                required
+              />
+              <Input
+                label="password"
+                type="password"
+                placeholder={t('Auth.password')}
+                register={register}
+                required
+              />
+              <IconButton
+                variant={IconButtonVariant.SECONDARY}
+                type="submit"
+                icon={
+                  status === 'pending' ? (
+                    <svg
+                      className="mr-1 h-5 w-5 animate-spin"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24">
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                  ) : (
+                    <div className="relative top-0.5">
+                      <Image
+                        width="24"
+                        height="16"
+                        src="/ZUT_logo_kolor.svg"
+                        alt="zut logo"
+                      />
+                    </div>
+                  )
+                }>
+                {t('Auth.login')}
+              </IconButton>
+              {status === 'failed' && error && (
+                <div className="rounded-lg bg-red-300 py-2 px-4 text-red-700">
+                  {error}
+                </div>
+              )}
+            </form>
           </div>
-        </div>
-        <div className="container mx-auto px-6">
-          <form
-            className="flex flex-col items-center justify-center space-y-6"
-            onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              label="username"
-              type="username"
-              placeholder={t('Auth.username')}
-              register={register}
-              required
+          <div className="my-16 flex items-center justify-center">
+            <Image
+              src={'/undraw_login_re_4vu2.svg'}
+              alt="login"
+              width="384"
+              height="229"
             />
-            <Input
-              label="password"
-              type="password"
-              placeholder={t('Auth.password')}
-              register={register}
-              required
-            />
-            <IconButton
-              variant={IconButtonVariant.SECONDARY}
-              type="submit"
-              icon={
-                status === 'pending' ? (
-                  <svg
-                    className="mr-1 h-5 w-5 animate-spin"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24">
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                  </svg>
-                ) : (
-                  <div className="relative top-0.5">
-                    <Image
-                      width="24"
-                      height="16"
-                      src="/ZUT_logo_kolor.svg"
-                      alt="zut logo"
-                    />
-                  </div>
-                )
-              }>
-              {t('Auth.login')}
-            </IconButton>
-            {status === 'failed' && error && (
-              <div className="rounded-lg bg-red-300 py-2 px-4 text-red-700">
-                {error}
-              </div>
-            )}
-          </form>
+          </div>
+          <Footer />
         </div>
-        <div className="my-16 flex items-center justify-center">
-          <Image
-            src={'/undraw_login_re_4vu2.svg'}
-            alt="login"
-            width="466"
-            height="330"
-          />
-        </div>
-        <Footer />
       </div>
     </>
   );
