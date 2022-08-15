096a3e0ee723f7832841268d7eb49794030e4eb0
Paweł Kałdus <xdk78888@gmail.com>
2022-08-15 11:47:27 +0200

    refactor Courses page to better match redux ducks pattern

---
+++ Front-End/src/features/courses/Courses.tsx
@@ -0,0 +1,131 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { BadgeCheckIcon, CheckCircleIcon } from '@heroicons/react/solid';
+import FancyCard from '@app/components/FancyCard';
+import { enrollCourse } from '@app/features/courses/enrollCourseSlice';
+import FancyToast from '@app/components/FancyToast';
+import DynamicCourseCard from '@app/components/DynamicCourseCard';
+import IconButton, { IconButtonVariant } from '@app/components/IconButton';
+import IconButtonLink, {
+  IconButtonLinkVariant,
+} from '@app/components/IconButtonLink';
+import type { Course } from '@app/models/Course';
+
+type CoursesProps = {
+  courses: Course[];
+  translations: (key: string) => string;
+};
+
+export default function Courses({ courses, translations }: CoursesProps) {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleEnrollCourse = (courseId: number) => async () => {
+    await dispatch(enrollCourse(courseId));
+    notify();
+    // wait 1 second to show the toast
+    setTimeout(() => {
+      toast.remove('course-enrolled-notification');
+      router.push(`/courses/${courseId}`);
+    }, 1000);
+  };
+
+  const notify = () =>
+    toast.custom(
+      (to) =>
+        to.visible && (
+          <FancyToast
+            message={translations('Courses.course-enrolled')}
+            toastObject={to}
+            className="border-indigo-500 bg-indigo-200 text-indigo-900"
+          />
+        ),
+      {
+        id: 'course-enrolled-notification',
+        position: 'top-center',
+        duration: 1000,
+      }
+    );
+
+  return (
+    <>
+      {courses && courses.length > 0 ? (
+        <div className="flex flex-col justify-center space-y-6 sm:flex-row sm:space-y-0 sm:space-x-12">
+          {courses.map((course) => (
+            <FancyCard
+              key={course.id}
+              title={course.name}
+              description={
+                <div className="flex flex-col">
+                  {course.description}
+                  <div>
+                    <div className="mt-4 mb-1 text-xs text-neutral-400">
+                      {translations('Manage.name')}
+                    </div>
+                    {course.lessons?.length > 0 &&
+                      course.lessons?.map((lesson) => (
+                        <div key={lesson.id}>
+                          <div className="text-indigo-900 dark:text-indigo-300">
+                            {lesson.name}
+                          </div>
+                        </div>
+                      ))}
+                    ...
+                  </div>
+                </div>
+              }
+              cardColor="bg-white"
+              shadowColor="shadow-black/25"
+              hoverShadowColor="hover:shadow-black/25"
+              bottomControls={
+                course.enrolled ? (
+                  <Link href={`/courses/enrolled`} passHref={true}>
+                    <IconButtonLink
+                      className="w-fit"
+                      variant={IconButtonLinkVariant.OUTLINE_PRIMARY}
+                      icon={<BadgeCheckIcon className="h-5 w-5" />}>
+                      {translations('Courses.enrolled')}
+                    </IconButtonLink>
+                  </Link>
+                ) : (
+                  <IconButton
+                    variant={IconButtonVariant.PRIMARY}
+                    icon={<CheckCircleIcon className="h-5 w-5" />}
+                    onClick={handleEnrollCourse(course.id)}>
+                    {translations('Courses.enroll')}
+                  </IconButton>
+                )
+              }
+            />
+          ))}
+          <DynamicCourseCard />
+        </div>
+      ) : (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <p className="pb-8 text-lg font-medium">
+            {translations('Courses.no-courses-found')}
+          </p>
+          <Image
+            src="/undraw_no_data_re_kwbl.svg"
+            alt="No data"
+            width={200}
+            height={200}
+          />
+        </div>
+      )}
+      {courses && courses.length > 0 && (
+        <div className="my-16 flex items-center justify-center">
+          <Image
+            src="/undraw_online_learning_re_qw08.svg"
+            alt="online_learning"
+            width="384"
+            height="276"
+          />
+        </div>
+      )}
+    </>
+  );
+}
--- Front-End/src/pages/courses/index.tsx
+++ Front-End/src/pages/courses/index.tsx
@@ -1,66 +1,28 @@
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
 import Head from 'next/head';
-import Link from 'next/link';
-import toast, { Toaster } from 'react-hot-toast';
-import { useRouter } from 'next/router';
+import { Toaster } from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
-import { BadgeCheckIcon, CheckCircleIcon } from '@heroicons/react/solid';
 import Footer from '@app/components/Footer';
 import NavBar from '@app/components/NavBar';
-import FancyCard from '@app/components/FancyCard';
 import { useAppSelector, useAuthRedirect } from '@app/hooks';
 import {
   fetchCourses,
   selectCoursesData,
 } from '@app/features/courses/coursesSlice';
 import { WEBSITE_TITLE } from '@app/constants';
-import { enrollCourse } from '@app/features/courses/enrollCourseSlice';
 import { wrapper } from '@app/store';
-import FancyToast from '@app/components/FancyToast';
-import DynamicCourseCard from '@app/components/DynamicCourseCard';
-import IconButton, { IconButtonVariant } from '@app/components/IconButton';
-import IconButtonLink, {
-  IconButtonLinkVariant,
-} from '@app/components/IconButtonLink';
+import Courses from '@app/features/courses/Courses';
 
 export default function CoursesPage() {
   const [user, isLoggedIn] = useAuthRedirect();
   const dispatch = useDispatch();
   const t = useTranslations();
   const courses = useAppSelector(selectCoursesData);
-  const router = useRouter();
-
-  const handleEnrollCourse = (courseId: string) => async () => {
-    await dispatch(enrollCourse(courseId));
-    notify();
-    // wait 1 second to show the toast
-    setTimeout(() => {
-      toast.remove('course-enrolled-notification');
-      router.push(`/courses/${courseId}`);
-    }, 1000);
-  };
-
-  const notify = () =>
-    toast.custom(
-      (to) =>
-        to.visible && (
-          <FancyToast
-            message={t('Courses.course-enrolled')}
-            toastObject={to}
-            className="border-indigo-500 bg-indigo-200 text-indigo-900"
-          />
-        ),
-      {
-        id: 'course-enrolled-notification',
-        position: 'top-center',
-        duration: 1000,
-      }
-    );
 
   if (!user && !isLoggedIn) {
     return null;
   }
+
   return (
     <>
       <Head>
@@ -89,81 +51,8 @@ export default function CoursesPage() {
           </div>
         </div>
         <div className="container mx-auto px-12">
-          {courses && courses.length > 0 ? (
-            <div className="flex flex-col justify-center space-y-6 sm:flex-row sm:space-y-0 sm:space-x-12">
-              {courses.map((course) => (
-                <FancyCard
-                  key={course.id}
-                  title={course.name}
-                  description={
-                    <div className="flex flex-col">
-                      {course.description}
-                      <div>
-                        <div className="mt-4 mb-1 text-xs text-neutral-400">
-                          {t('Manage.name')}
-                        </div>
-                        {course.lessons?.length > 0 &&
-                          course.lessons?.map((lesson) => (
-                            <div key={lesson.id}>
-                              <div className="text-indigo-900 dark:text-indigo-300">
-                                {lesson.name}
-                              </div>
-                            </div>
-                          ))}
-                        ...
-                      </div>
-                    </div>
-                  }
-                  cardColor="bg-white"
-                  shadowColor="shadow-black/25"
-                  hoverShadowColor="hover:shadow-black/25"
-                  bottomControls={
-                    course.enrolled ? (
-                      <Link href={`/courses/enrolled`} passHref={true}>
-                        <IconButtonLink
-                          className="w-fit"
-                          variant={IconButtonLinkVariant.OUTLINE_PRIMARY}
-                          icon={<BadgeCheckIcon className="h-5 w-5" />}>
-                          {t('Courses.enrolled')}
-                        </IconButtonLink>
-                      </Link>
-                    ) : (
-                      <IconButton
-                        variant={IconButtonVariant.PRIMARY}
-                        icon={<CheckCircleIcon className="h-5 w-5" />}
-                        onClick={handleEnrollCourse(course.id)}>
-                        {t('Courses.enroll')}
-                      </IconButton>
-                    )
-                  }
-                />
-              ))}
-              <DynamicCourseCard />
-            </div>
-          ) : (
-            <div className="flex h-full w-full flex-col items-center justify-center">
-              <p className="pb-8 text-lg font-medium">
-                {t('Courses.no-courses-found')}
-              </p>
-              <Image
-                src="/undraw_no_data_re_kwbl.svg"
-                alt="No data"
-                width={200}
-                height={200}
-              />
-            </div>
-          )}
+          <Courses courses={courses} translations={t} />
         </div>
-        {courses && courses.length > 0 && (
-          <div className="my-16 flex items-center justify-center">
-            <Image
-              src="/undraw_online_learning_re_qw08.svg"
-              alt="online_learning"
-              width="384"
-              height="276"
-            />
-          </div>
-        )}
         <Toaster />
         <Footer />
       </div>
