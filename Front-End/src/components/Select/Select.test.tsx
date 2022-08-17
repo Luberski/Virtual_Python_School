7c4d187b6264407b98eef4a470cc0a14b5da4290
Luberski <Luberski@int.pl>
2022-08-17 12:20:21 +0200

    Select component: Storybook, Unit tests.

---
+++ Front-End/src/components/Select/Select.test.tsx
@@ -0,0 +1,44 @@
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Select from '.';
+
+const optList = [
+  { id: 1, value: 'Opt1', unavailable: false },
+  { id: 2, value: 'Opt2', unavailable: false },
+];
+
+const setup = () => {
+  render(<Select optionsList={optList}></Select>);
+};
+
+describe('Select', () => {
+  it('checks option selected when user chooses first option', () => {
+    setup();
+
+    const openListbox = screen.getByRole('button', {
+      name: /select option.../i,
+    });
+
+    fireEvent.click(openListbox);
+    const listboxElem = screen.getByRole('listbox');
+    const option1 = screen.getAllByRole('option')[0];
+
+    userEvent.selectOptions(listboxElem, 'Opt1');
+    fireEvent.click(option1);
+    expect(listboxElem).toHaveTextContent('Opt1');
+    expect(openListbox).toHaveTextContent('Opt1');
+  });
+
+  it('check option selected if user selected no option', () => {
+    setup();
+
+    const openListbox = screen.getByRole('button', {
+      name: /select option.../i,
+    });
+
+    fireEvent.click(openListbox);
+    fireEvent.click(openListbox);
+    expect(openListbox).toHaveTextContent('Select option...');
+  });
+});
