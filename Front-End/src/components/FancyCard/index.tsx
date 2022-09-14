789ceaf7521aa1e734c234bbe5e3c099727b8618
Paweł Kałdus <xdk78888@gmail.com>
2022-09-14 17:28:54 +0200

    update deps, fix Hydration error

--- Front-End/src/components/FancyCard/index.tsx
+++ Front-End/src/components/FancyCard/index.tsx
@@ -22,8 +22,8 @@ export default function FancyCard({
   return (
     <div
       className={`flex flex-col justify-between p-6 ${cardColor} ${darkCardColor} brand-shadow2 border shadow-black/25 dark:border-neutral-400 ${hoverShadowColor} transition duration-500 ease-in-out ${shadowColor} max-h-96 w-80 rounded-lg text-neutral-700 dark:text-neutral-100`}>
-      <p className="break-words text-2xl font-bold">{title}</p>
-      <p className="truncate break-words">{description}</p>
+      <div className="break-words text-2xl font-bold">{title}</div>
+      <div className="truncate break-words">{description}</div>
       <div className="mt-6">{bottomControls}</div>
     </div>
   );
