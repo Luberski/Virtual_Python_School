c39d37ad46c4e0750448883fd03ba89c7ff64fa3
Paweł Kałdus <xdk78888@gmail.com>
2022-11-19 15:33:10 +0100

    load api base url form .env

--- Front-End/src/apiClient.ts
+++ Front-End/src/apiClient.ts
@@ -1,10 +1,6 @@
 import ky from 'ky-universal';
 
-let BASE_URL = `http://localhost:3000/api`;
-if (process.env.NODE_ENV === 'production') {
-  // TODO: move BASE_URL to .env
-  BASE_URL = `http://virtualschool.wi.zut.edu.pl/api`;
-}
+const BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000/api';
 
 export default ky.extend({
   prefixUrl: BASE_URL,
